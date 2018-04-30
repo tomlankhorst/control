3942aaa4ef39bddef5de7f08f16508b75fa20d9c
Tom Lankhorst <info@tomlankhorst.nl>
2018-04-30 09:13:05 +0200

    Documentation

--- include/control/system/ss.h
+++ include/control/system/ss.h
@@ -12,6 +12,18 @@
 
 namespace control { namespace system {
 
+/**
+ * State-space 
+ *
+ * Discretizes LTI (MIMO) state-space system.
+ * Based upon Eigen matrix math lib. 
+ * Uses fixed-size matrices and vectorization. 
+ *
+ * @tparam T storage-type
+ * @tparam Nx number of states
+ * @tparam Nu number of inputs
+ * @tparam Ny number of outputs
+ */
 template<typename T, size_t Nx, size_t Nu=1, size_t Ny=1>
 class ss {
 public:
@@ -31,14 +43,35 @@ private:
 
 public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+  /**
+   * Construct a new SS and initialize the output and state to zero
+   *
+   * @param TA A state-transfer matrix
+   * @param TB B input matrix
+   * @param TC C output matrix
+   * @param TD D feed-through matrix
+   */
   ss(TA A, TB B, TC C, TD D) : A{A}, B{B}, C{C}, D{D} {
     x = Tx::Zero();
     y = Ty::Zero();
   }
 
+  /**
+   * @var Tx current state of the system
+   */
 	Tx x;
+
+  /**
+   * @var Ty current output of the system
+   */
 	Ty y;
 
+  /**
+   * Step the system
+   *
+   * @param Tu u input 
+   * @return Ty output
+   */
 	Ty step(Tu u){
 		x = A*x + B*u;
 		y = C*x + D*u;
