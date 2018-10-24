ccf2cc2eef0314a3ea489227bb76ca9a94738b1d
Tom Lankhorst <hello@tomlankhorst.nl>
2018-10-24 10:23:42 +0200

    Eigen3  cache

--- ci/travis-install-eigen3.sh
+++ ci/travis-install-eigen3.sh
@@ -3,9 +3,17 @@
 
 set -ex
 
-wget http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2
-tar -xvf 3.3.4.tar.bz2
-rm 3.3.4.tar.bz2
+DIR=$HOME/local/include/eigen3
+if [ -d $DIR ]; then
+  echo "Dir $DIR exists. Skipping."
+  exit 0
+else
+  echo "Dir $DIR does not exist. Building."
+fi
+
+wget -O eigen.tar.bz2 http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2
+tar -xvf eigen.tar.bz2
+rm eigen.tar.bz2
 cd eigen-eigen-5a0156e40feb
 mkdir build
 cd build
