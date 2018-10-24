074ab2cb38c4b40dbdba13d7cecaae86cfeb3269
Tom Lankhorst <hello@tomlankhorst.nl>
2018-10-24 09:51:17 +0200

    Source build

--- ci/travis-install-doxygen.sh
+++ ci/travis-install-doxygen.sh
@@ -3,12 +3,14 @@
 
 set -ex
 
-git clone https://github.com/doxygen/doxygen.git
-cd doxygen
+wget -O doxygen.tar.gz https://github.com/doxygen/doxygen/archive/Release_1_8_14.tar.gz
+tar -xvf doxygen.tar.gz
+rm doxygen.tar.gz
+cd doxygen-Release_1_8_14
 mkdir build
 cd build
 cmake -G "Unix Makefiles" ..
 make
 sudo make install
 cd ..
-rm -rf doxygen
+rm -rf doxygen-Release_1_8_14
