03d578fb442fb8ea98e526fb0ab545b745cde6d0
Tom Lankhorst <info@tomlankhorst.nl>
2020-07-07 08:41:29 +0200

    Bug-fixes

--- include/control/classic/pid_discrete_integration.h
+++ include/control/classic/pid_discrete_integration.h
@@ -1,7 +1,11 @@
-#include <type_traits>
 #include <iostream>
+#include <type_traits>
 
 namespace control::classic {
+
+#include <iostream>
+
+#include <type_traits>
 
 template<typename T>
 class basic_pid {
@@ -19,21 +23,21 @@ public:
     };
 
     struct config {
-        // Timestep
-        const T Ts;
         // Proportional, Integral, Derivate gain
         const T Kp = 1, Ki = 0, Kd = 0;
         // Filter time-step
         const T Tf = 0;
+        // Timestep
+        const T Ts = 1;
         // Discrete integrator formula
-        const IntegratorFormula integrator_formula = IntegratorFormula::ForwardEuler;
+        const IntegratorFormula integrator_formula = IntegratorFormula::Trapezoidal;
     };
 
     explicit constexpr basic_pid (const config& c) : 
         coeff_ { calculate_coeff(c) } {}
 
-    T operator()(T u) noexcept {
-        T y;
+    constexpr T operator()(T u) noexcept {
+        T y = 0;
 
         /* Direct form II transposed */
         y = u * coeff_.b[0] + state_.w[0];
@@ -56,47 +60,51 @@ private:
         switch (c.integrator_formula) {
             case IntegratorFormula::BackwardEuler:
                 return backward_euler_coeff(c);
-            case IntegratorFormula::Trapezoidal:
-                return trapezoidal_coeff(c);
             case IntegratorFormula::ForwardEuler:
-            default:
                 return forward_euler_coeff(c);
+            case IntegratorFormula::Trapezoidal:
+            default:
+                return trapezoidal_coeff(c);
         }
     }
 
     static constexpr coeff_t forward_euler_coeff(const config& c) {
-        const auto& [Ts, Kp, Ki, Kd, Tf, _] = c;
+        const auto& [Kp, Ki, Kd, Tf_, Ts, _] = c;
+        // ForwardEuler: Kp + Kd/(Tf + Ts/(z - 1)) + (Ki*Ts)/(z - 1)
+        const auto Tf = Kd ? Tf_ : 1; // If Kd not set, set Tf to 1 for stability
         return {{
-             (Kd + Ki*Ts*Ts + Kp*Tf - Kp*Ts - Ki*Tf*Ts)/(Tf - Ts),
-             (Kp*Ts - 2*Kp*Tf - 2*Kd + Ki*Tf*Ts)/(Tf - Ts),
-             (Kd + Kp*Tf)/(Tf - Ts)
+            (Kd + Kp*Tf)/Tf,
+            (Kp*Ts - 2*Kp*Tf - 2*Kd + Ki*Tf*Ts)/Tf,
+            (Kd + Ki*Ts*Ts + Kp*Tf - Kp*Ts - Ki*Tf*Ts)/Tf,
         }, {
-            (Ts - 2*Tf)/(Tf - Ts),
-            Tf/(Tf - Ts),
+            (Ts - 2*Tf)/Tf,
+            (Tf - Ts)/Tf,
         }};
     }
 
     static constexpr coeff_t backward_euler_coeff(const config& c) {
-        const auto& [Ts, Kp, Ki, Kd, Tf, _] = c;
+        const auto& [Kp, Ki, Kd, Tf, Ts, _] = c;
+        // BackwardEuler: Kp + Kd/(Tf + (Ts*z)/(z - 1)) + (Ki*Ts*z)/(z - 1)
         return {{
-            (Kd + Ki*Ts*Ts + Kp*Tf - Kp*Ts - Ki*Tf*Ts)/(Tf - Ts),
-            (Kp*Ts - 2*Kp*Tf - 2*Kd + Ki*Tf*Ts)/(Tf - Ts),              
-            (Kd + Kp*Tf)/(Tf - Ts),
+            (Kd + Ki*Ts*Ts + Kp*Tf + Kp*Ts + Ki*Tf*Ts)/(Tf + Ts),
+            -(2*Kd + 2*Kp*Tf + Kp*Ts + Ki*Tf*Ts)/(Tf + Ts),
+            (Kd + Kp*Tf)/(Tf + Ts),
         }, {
-            (Ts - 2*Tf)/(Tf - Ts),
-            Tf/(Tf - Ts)
+            -(2*Tf + Ts)/(Tf + Ts),
+            Tf/(Tf + Ts),
         }};
     }
 
     static constexpr coeff_t trapezoidal_coeff(const config& c) {
-        const auto& [Ts, Kp, Ki, Kd, Tf, _] = c;
-        return {{
-             (4*Kd + Ki*Ts*Ts + 4*Kp*Tf - 2*Kp*Ts - 2*Ki*Tf*Ts)/(4*Tf - 2*Ts),
-             -(- Ki*Ts*Ts + 4*Kd + 4*Kp*Tf)/(2*Tf - Ts),
-             (4*Kd + Ki*Ts*Ts + 4*Kp*Tf + 2*Kp*Ts + 2*Ki*Tf*Ts)/(4*Tf - 2*Ts)
+        const auto& [Kp, Ki, Kd, Tf, Ts, _] = c;
+        // Trapezoidal: Kp + Kd/(Tf + (Ts*(z + 1))/(2*(z - 1))) + (Ki*Ts*(z + 1))/(2*(z - 1))
+        return  {{
+	        (4*Kd + Ki*Ts*Ts + 4*Kp*Tf + 2*Kp*Ts + 2*Ki*Tf*Ts)/(4*Tf + 2*Ts),
+	        -(4*Kd - Ki*Ts*Ts + 4*Kp*Tf)/(2*Tf + Ts),
+	        (4*Kd + Ki*Ts*Ts + 4*Kp*Tf - 2*Kp*Ts - 2*Ki*Tf*Ts)/(4*Tf + 2*Ts),
         }, {
-            -(4*Tf)/(2*Tf - Ts),
-            (2*Tf + Ts)/(2*Tf - Ts)
+	        -(4*Tf)/(2*Tf + Ts),
+	        (2*Tf - Ts)/(2*Tf + Ts),
         }};
     }
 
